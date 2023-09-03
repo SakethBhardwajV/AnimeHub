098550ad7b83b1ffd89074bb17c2d08beea01394
sakethbhardwajV <sakethb.vangipuram@gmail.com>
2023-09-03 13:22:31 +0530

    setup home page

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 import express from "express";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://api.jikan.moe/v4/recommendations/anime"
+    );
+    const result = response.data;
+    res.render("index.ejs", {
+      res: result,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
 app.listen(port, () => {
